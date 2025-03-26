d6b3b225b98f9936df06d321b2051c927fb74223
jakubstec <kub9stec@gmail.com>
2025-03-26 13:59:03 +0100

    test my branch

--- backend/server.js
+++ backend/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}!! testing my own branch`);
 });
