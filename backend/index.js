1ab9bb1474b8db53344c309e5635990682f86cc9
filipjarzyna <filipjarzyna@protonmail.com>
2025-05-20 16:29:48 +0200

    dockerize backend

--- backend/index.js
+++ backend/index.js
@@ -21,44 +21,44 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/login', function (req, res) {
-  const client_id = process.env.SPOTIFY_CLIENT_ID;
-  const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
-  const state = "4hdkjhgfjkldasj;l";
-  const scope = [
-    'user-read-private',
-    'user-read-email',
-    'user-read-currently-playing',
-    'user-read-playback-state',
-    'user-modify-playback-state',
-    'user-read-recently-played',
-    'user-library-read',
-    'playlist-read-private',
-    'playlist-read-collaborative',
-    'streaming'
-  ].join(' ');
+// app.get('/login', function (req, res) {
+//   const client_id = process.env.SPOTIFY_CLIENT_ID;
+//   const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
+//   const state = "4hdkjhgfjkldasj;l";
+//   const scope = [
+//     'user-read-private',
+//     'user-read-email',
+//     'user-read-currently-playing',
+//     'user-read-playback-state',
+//     'user-modify-playback-state',
+//     'user-read-recently-played',
+//     'user-library-read',
+//     'playlist-read-private',
+//     'playlist-read-collaborative',
+//     'streaming'
+//   ].join(' ');
   
   
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
-  });
+//   res.redirect('https://accounts.spotify.com/authorize?' +
+//     querystring.stringify({
+//       response_type: 'code',
+//       client_id: client_id,
+//       scope: scope,
+//       redirect_uri: redirect_uri,
+//       state: state
+//     }));
+//   });
   
   
   
-  app.get('/callback', async function (req, res) {
-    console.log("callback")
-    const code = req.query.code;
-    const AccessTokenResponse = await getSpotifyAccessToken(process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET, code, process.env.SPOTIFY_REDIRECT_URI);
-    const track = await getCurrentlyPlayingTrack(AccessTokenResponse.access_token);
-    console.log(track.track.name);
+//   app.get('/callback', async function (req, res) {
+//     console.log("callback")
+//     const code = req.query.code;
+//     const AccessTokenResponse = await getSpotifyAccessToken(process.env.SPOTIFY_CLIENT_ID, process.env.SPOTIFY_CLIENT_SECRET, code, process.env.SPOTIFY_REDIRECT_URI);
+//     const track = await getCurrentlyPlayingTrack(AccessTokenResponse.access_token);
+//     console.log(track.track.name);
     
-  });
+//   });
   
   
   
@@ -77,6 +77,8 @@ app.get('/login', function (req, res) {
     if (!track) {
       return res.status(204).send(); // No track playing
     }
+
+    
 
     res.json(track);
   } catch (error) {
@@ -111,7 +113,7 @@ app.post('/exchange-token', async function (req, res) {
 
     // add user to the database
     await Database.addNewUser(user.id, user.name, token_hash);
-    
+
     res.status(200).json({
       access_token: tokens.access_token,
       refresh_token: tokens.refresh_token,
@@ -152,7 +154,6 @@ app.post('/refresh-token', async function (req, res) {
     res.status(500).json({ error: 'Failed to refresh token' });
   }
 });
-
 
 
 
