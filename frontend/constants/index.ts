00b25438f2ec68174a8e76d49812b7e266871d17
filipjarzyna <filipjarzyna@protonmail.com>
2025-06-13 14:30:01 +0200

    added images

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,22 +1,25 @@
-import remove from "@/assets/images/remove.png"
+// import remove from "@/assets/images/remove.png"
+import welcome_image01 from "@/assets/images/welcome_image01.jpg"
+import welcome_image02 from "@/assets/images/welcome_image02.png"
+import welcome_image03 from "@/assets/images/welcome_image03.png"
 
 export const onboarding = [
     {
         id: 1,
         title: 'Log in to Your Spotify Account',
         description: 'Connect to start exploring music around you.',
-        image: remove,
+        image: welcome_image03,
     },
     {
         id: 2,
         title: 'Explore a Map of Music Hotspots',
         description: 'See where music is trending nearby.',
-        image: remove,
+        image: welcome_image02,
     },
     {
         id: 3,
         title: 'Tap to See What Users Are Listening To',
         description: 'Discover tracks people are playing right now.',
-        image: remove,
+        image: welcome_image01,
     }
 ]
